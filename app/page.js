4c3d5fd225b91ae5e4873d052068aa149092a95a
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-08-12 11:33:32 -0700

    deply succeccfily

--- app/page.js
+++ app/page.js
@@ -3,7 +3,11 @@ import Image from 'next/image'
 export default function Home() {
   return (
    <>
-    <h1>Hellow World </h1>
+   <div className=''>
+
+    <h1 className='text-center mt-5 text-4xl font-bold'>Hellow NextJS</h1>
+    <h1 className='text-center mt-5'>Deploy on Vercel</h1>
+   </div>
    </>
   )
 }
